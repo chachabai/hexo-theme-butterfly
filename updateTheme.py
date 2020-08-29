16d77de3582910f3d448b129875674b615bd83f5
chachabai <dna049@outlook.com>
2020-08-30 04:00:32 +0800

    update

--- updateTheme.py
+++ updateTheme.py
@@ -4,6 +4,8 @@ import sys
 os.system('git add .')
 if len(sys.argv) < 3:
 	os.system('git commit -m "update"')
+	print("HH");
 else:
-	os.system('git commit -m "' + argv[2] + '"')
+	os.system('git commit -m "' + sys.argv[2] + '"')
+	print('git commit -m "' + sys.argv[2] + '"');
 os.system('git push origin master')
